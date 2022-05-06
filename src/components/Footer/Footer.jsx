586975e4b3e661a7992efb0fd03ff5beeafe616a
Emanuelsalas <emmasalas1@hotmail.com>
2022-05-06 17:00:27 -0600

    "Footer"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,24 @@
 import './Footer.css'
-
+import Card from 'react-bootstrap/Card'
 
 export default function Footer(){
     return(
         <footer>
-            
+            <Card>
+            <Card.Footer>Logo</Card.Footer>
+            <Card.Body >  
+                <Card.Text>
+                      ©ConociendoGuanacaste, Inc. 2022. 
+                </Card.Text>
+                <Card.Text>
+                Correo electrónico: ConociendomiGuanacaste@gmai.com
+                </Card.Text>
+                <Card.Text>
+                Contacto directo: 506 2495 0972
+                </Card.Text>
+                
+            </Card.Body>
+            </Card>
         </footer>
     )
 }
