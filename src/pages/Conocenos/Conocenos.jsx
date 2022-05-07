0f7ec95d8c86567cc46e85dad90295e2447aae1e
JordanGS01 <jordan-guzman-salas@hotmail.com>
2022-05-07 11:22:04 -0600

    Carrusel imagenes Conocenos

--- src/pages/Conocenos/Conocenos.jsx
+++ src/pages/Conocenos/Conocenos.jsx
@@ -1,10 +1,45 @@
 import './Conocenos.css'
+import Carousel from 'react-bootstrap/Carousel'
 
 
 export default function Conocenos(){
     return(
         <>
-            <p>Apartado conócenos</p>
+        <Carousel touch={true}>
+            <Carousel.Item interval={3000}>
+                <img
+                className="d-block w-100"
+                src="https://staticuestudio.blob.core.windows.net/buhomag/2016/10/06120007/everest1.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={3000}>
+                <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1585409677983-0f6c41ca9c3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80"
+                alt="Second slide"
+                />
+                <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={3000}>
+                <img
+                className="d-block w-100"
+                src="https://st.depositphotos.com/1563944/2419/i/600/depositphotos_24199705-stock-photo-three-close-mountain-peak.jpg"
+                alt="Third slide"
+                />
+                <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
         </>
     )
 }
