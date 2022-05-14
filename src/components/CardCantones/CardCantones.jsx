fe877e8cc640ec656184a39795c79fd54b8e4863
Emanuelsalas <emmasalas1@hotmail.com>
2022-05-13 21:09:09 -0600

    CardCantones

---
+++ src/components/CardCantones/CardCantones.jsx
@@ -0,0 +1,15 @@
+import './CardCantones.css'
+import Card from 'react-bootstrap/Card'
+import { Link } from 'react-router-dom';
+export default function CardCantones(props){
+    return(
+    <Card id='Cantones' as={Link} to={props.link} style={{ width: '18rem' }} >
+        <Card.Img variant="top" src= {props.src} />
+        <Card.Body id='body'>
+            <Card.Title >{props.nombre}</Card.Title>
+            {props.descripcion}
+        </Card.Body>
+    </Card>
+    
+    )
+}
